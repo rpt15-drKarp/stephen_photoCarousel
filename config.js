a888da5ccaf385ac18b185d02bca2b39a4076ab7
Stephen Jeong <stephen.h.jeong@gmail.com>
2019-11-11 20:16:39 -0800

    update mysql url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   appUrl: 'ec2-54-193-107-57.us-west-1.compute.amazonaws.com',
-  dbUrl: 'ec2-52-53-179-7.us-west-1.compute.amazonaws.com',
+  dbUrl: 'ec2-54-193-76-115.us-west-1.compute.amazonaws.com',
   redis: 'ec2-54-219-143-187.us-west-1.compute.amazonaws.com',
   servers: [
     'ec2-52-53-125-40.us-west-1.compute.amazonaws.com:3100',
